11600b534f57043266bd7e01c0e1c3b7f57209f1
ehdwo13 <ehdwo13@gmail.com>
2024-08-16 16:53:50 +0900

    modify profile display logic

--- src/main/java/www/project/config/WebMvcConfig.java
+++ src/main/java/www/project/config/WebMvcConfig.java
@@ -11,16 +11,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         String os = System.getProperty("os.name").toLowerCase();
         String homeDir = System.getProperty("user.home");
         String UP_DIR = "";
-
         if(os.contains("win")) {
-            UP_DIR = homeDir + "\\image\\";
+            UP_DIR = "C:\\image\\";
         } else {
             UP_DIR = homeDir + "/image/";
         }
-
         return "file:" + UP_DIR;
     }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
